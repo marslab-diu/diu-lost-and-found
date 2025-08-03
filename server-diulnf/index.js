5383910c5fc583c4bd1e43118955d19436a1385b
Sourav Garodia <96367006+garodiaa@users.noreply.github.com>
2025-08-03 19:01:52 +0600

    Server side initial setup with mongodb

---
+++ server-diulnf/index.js
@@ -0,0 +1,51 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
+const port = process.env.PORT || 3000;
+
+
+require("dotenv").config();
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.1gc3l46.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+
+
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+
+
+
+async function run() {
+    try {
+        await client.connect();  
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+
+
+        app.get("/test", async (req, res) => {
+            res.send("Test route is working");
+        });
+    } finally {
+        // await client.close();
+    }
+}
+
+run().catch(console.dir);
+
+
+app.get("/", (req, res) => {
+  res.send("DIU-lnf server is running");
+});
+
+app.listen(port, () => {
+  console.log(`User server is running on port ${port}`);
+});
